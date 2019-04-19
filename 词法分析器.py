5b0e15ecb6e6f9bad7113163419752db055b2852
Oumourin <32457575+Oumourin@users.noreply.github.com>
2019-04-19 15:50:59 +0800

    V1.1 bug待修改

--- 词法分析器.py
+++ 词法分析器.py
@@ -45,22 +45,10 @@ def lexical_analysis(analysis_str: str):
     # analysis_str_copy = analysis_str
     to_be_process = []
     result_str = '词法分析结果：\n'
-    space = analysis_str.find(' ')
-    # for ptr in analysis_str:
-    #     if ptr == ',':
-    #         result_str = result_str + ',                 标号,'
-    #     elif ptr == ';':
-    #         result_str = result_str + ';                 标号;'
-    #     elif ptr == '+':
-    #         result_str = result_str + '+                  '
-    # while space != -1:
-    #     space = analysis_str.find(' ')
-    #     to_be_process.append(analysis_str[0:space])
-    #     # print(analysis_str[0:space])
-    #     analysis_str = analysis_str[space+2:]
-    #     analysis_str.find(' ')
+    # space = analysis_str.find(' ')
     to_be_process = analysis_str.split()
     for ptr in to_be_process:
+        ptr_len = len(ptr)
         if ptr.isdigit():
             temp_str = ptr + '            数字\n'
             result_str += temp_str
@@ -73,38 +61,11 @@ def lexical_analysis(analysis_str: str):
                     temp_str = ptr + '            标识符\n'
                     result_str += temp_str
             else:
-                # #  大小写混合字符串处理
-                # process_str = ptr
-                # lower_str = ''
-                # upper_str = ''
-                # local = 0
-                # length = len(process_str)
-                # while local < length:
-                #     if 'a' <= ptr[local] <= 'z':
-                #         process_str = ptr[local:]
-                #         process_str_add = process_str + ' '
-                #         for lower in process_str_add:
-                #             if 'a' <= lower <= 'z':
-                #                 lower_str += lower
-                #                 local += 1
-                #             else:
-                #                 temp_str = lower_str + '         标识符\n'
-                #                 result_str += temp_str
-                #                 lower_str = ''
-                #                 break
-                #     else:
-                #         process_str = ptr[local:]
-                #         process_str_add = process_str + ' '
-                #         for upper in process_str_add:
-                #             if 'A' <= upper <= 'Z':
-                #                 upper_str += upper
-                #                 local += 1
-                #             else:
-                #                 temp_str = upper_str + '       非法输入符号\n'
-                #                 result_str += temp_str
-                #                 upper_str = ''
-                #                 break
                 result_str = mix_upper_lower(ptr, result_str)
+        elif ptr.isalnum():
+            if '0' <= ptr[0] <= '9':
+                temp_str += ptr + '               非法输入符号,标识符不能以数字开头\n'
+                result_str += temp_str
         elif len(ptr) == 1:
             if ptr == ',':
                 temp_str += ptr + '             标号,\n'
@@ -124,14 +85,88 @@ def lexical_analysis(analysis_str: str):
             elif ptr == ':':
                 temp_str += ptr + '               标号:\n'
                 result_str += temp_str
-        elif len(ptr) == 2:
+        elif len(ptr) >= 2:
             if ptr == ':=':
                 temp_str += ptr + '               标号:= \n'
                 result_str += temp_str
-        elif ptr.isalnum():
-            if '0' <= ptr[0] <= '9':
-                temp_str += ptr + '               非法输入符号,标识符不能以数字开头\n'
-                result_str += temp_str
+            else:
+                local = 0
+                pre_local = local
+                # ptr_copy = ptr
+                while local < ptr_len:
+                    if 'a' <= ptr[local] <= 'z':
+                        if ptr[local] != 'v' or ptr[local+1] != 'a' or ptr[local+2] != 'r':
+                            local += 1
+                            while 'a' <= ptr[local] <= 'z' or '0' <= ptr[local] <= '9':
+                                local += 1
+                            temp_str = ptr[pre_local:local] + '              标识符'
+                            result_str += temp_str
+                            pre_local = local
+                        elif 'a' <= ptr[local+3] <= 'z' or '0' <= ptr[local+3] <= '9':
+                            local += 4
+                            while 'a' <= ptr[local] <= 'z' or '0' <= ptr[local] <= '9':
+                                local += 1
+                            temp_str = ptr[pre_local:local] + '            标识符'
+                            result_str += temp_str
+                            pre_local = local
+                            # ptr_copy = ptr_copy[local:]
+                        else:
+                            temp_str = 'var' + '         关键字'
+                            result_str += temp_str
+                            local += 3
+                            pre_local = local
+                            # ptr_copy = ptr_copy[local:]
+                    elif '1' <= ptr[local] <= '9':
+                        local += 1
+                        while '0' <= ptr[local] <= '9':
+                            local += 1
+                        if 'a' <= ptr[local] <= 'z':
+                            temp_str = ptr[pre_local:local] + '         标识符不能以数字开头'
+                            result_str += temp_str
+                            pre_local = local
+                        temp_str = ptr[pre_local:local] + '         数字'
+                        result_str += temp_str
+                        pre_local = local
+                    elif ptr[local] == ',':
+                        temp_str += ptr[pre_local:local] + '             标号,\n'
+                        result_str += temp_str
+                        local += 1
+                        pre_local = local
+                    elif ptr[local] == ';':
+                        temp_str += ptr[pre_local:local] + '             标号;\n'
+                        result_str += temp_str
+                        local += 1
+                        pre_local = local
+                    elif ptr[local] == '(':
+                        temp_str += ptr[pre_local:local] + '             标号(\n'
+                        result_str += temp_str
+                        local += 1
+                        pre_local = local
+                    elif ptr[local] == ')':
+                        temp_str += ptr[pre_local:local] + '              标号)\n'
+                        result_str += temp_str
+                        local += 1
+                        pre_local = local
+                    elif ptr[local] == '+':
+                        temp_str += ptr[pre_local:local] + '              标号+\n'
+                        result_str += temp_str
+                        local += 1
+                        pre_local = local
+                    elif ptr[local] == ':':
+                        if ptr[local+1] == '=':
+                            local += 2
+                            temp_str += ptr[pre_local:local] + '               标号:=\n'
+                            result_str += temp_str
+                            pre_local = local
+                        else:
+                            temp_str += ptr[pre_local:local] + '               标号:\n'
+                            result_str += temp_str
+                            local += 1
+                            pre_local = local
+                    # elif ptr.isalnum():
+                    #     if '0' <= ptr[0] <= '9':
+                    #         temp_str += ptr + '               非法输入符号,标识符不能以数字开头\n'
+                    #         result_str += temp_str
         else:
             temp_str += ptr + '                非法输入符号:\n'
             result_str += temp_str
